b87d2068af6de29d7905508f634e3483c9ba852f
kyicho <kyicho@outlook.com>
2020-08-30 20:25:17 -0400

    Implement HigherOrder compomponent for CalendarImport,AddService, Deposit forms Impelemented HigherOrder compomponent for CalendarImport,AddService, Deposit forms and made use of it in HomePage

--- src/views/home-page/HomePage.tsx
+++ src/views/home-page/HomePage.tsx
@@ -1,17 +1,19 @@
 import styles from "./HomePage.module.scss";
 
-import React from "react";
 import { connect } from "react-redux";
 import IStore from "../../models/IStore";
 import ShowsAction from "../../stores/shows/ShowsAction";
 import Actors from "./components/actors/Actors";
 import MainOverview from "./components/main-overview/MainOverview";
-import { Divider, Icon, Header } from "semantic-ui-react";
+import { Divider, Icon, Header, Button } from "semantic-ui-react";
 import { ReduxProps } from "../../models/ReduxProps";
 import LoadingIndicator from "../components/loading-indicator/LoadingIndicator";
 import { selectRequesting } from "../../selectors/requesting/RequestingSelector";
 import { Container } from "semantic-ui-react";
 import LandingPageHeading from "../components/main-nav/components/LandingPageHeading";
+import { runInThisContext } from "vm";
+import React from "react";
+import BareMinimumInformation from "./components/bare-min-information/BareMinimumInformation"
 
 interface IProps {}
 interface IState {}
@@ -27,16 +29,22 @@ const mapStateToProps = (state: IStore, ownProps: IProps): IStateToProps => ({
   ])
 });
 
+
 class HomePage extends React.Component<
   IProps & IStateToProps & ReduxProps<any, IRouteParams>,
   IState
 > {
+                  
   public render(): JSX.Element {
     const { isRequesting } = this.props;
-
+    
     return (
       <div className={styles.wrapper}>
-        <Container style={{ margin: 20 }}>
+        <Container style={{ margin: 200 }}>
+          <BareMinimumInformation />
+        
+        
+          {/*
           <LoadingIndicator isActive={isRequesting}>
             <MainOverview />
             <Divider horizontal={true}>
@@ -46,6 +54,7 @@ class HomePage extends React.Component<
             </Divider>
             <Actors />
           </LoadingIndicator>
+        */}
         </Container>
       </div>
     );
